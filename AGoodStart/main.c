2ccbca2afd0771cfd9eab1b610731d468285265b
Edwin Cardenas <eacardenase@gmail.com>
2024-12-11 12:46:15 -0500

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Edwin Cardenas on 12/11/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

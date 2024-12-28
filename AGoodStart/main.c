872d15badb070ced7e1103a462856d7c707d45d9
Edwin Cardenas <eacardenase@gmail.com>
2024-12-28 14:29:58 -0500

    refactor: use c strings instead of literals

--- AGoodStart/main.c
+++ AGoodStart/main.c
@@ -8,9 +8,13 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    // Write the beginning of the novel
+    char *firstLine = "It was the best of times.\n";
+    char *secondLine = "It was the worst of times.\n";
+    
     // Print the beginning of the novel
-    printf("It was the best of times.\n");
-    printf("It was the worst of times.\n");
+    printf("%s", firstLine);
+    printf("%s", secondLine);
     /*  Is that actually any good?
         Maybe it needs a rewrite. */
     
